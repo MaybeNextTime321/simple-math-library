56cf860caf227325e07161b2b63e6d17c24e0021
Ilia Rybakov <47.2region@gmail.com>
2026-05-19 07:34:53 +0000

    math library update

--- customMath.h
+++ customMath.h
@@ -1,42 +1,41 @@
 #pragma once
-#include <iostream>
 #include <cmath>
 #include <climits>
 
 namespace math
 {
 
-	enum class MathStatus
+        enum class MathStatus
 
-	{
-		Ok,
-		DivideByZero,
-		Overflow,
-		FactorialFromNegative,
-		ParseError,
-		Help
-	};
+        {
+                Ok,
+                DivideByZero,
+                Overflow,
+                FactorialFromNegative,
+                ParseError,
+                Help
+        };
 
-	int add(int firstNumber, int secondNumber, MathStatus& operationStatus)                            
-	{       
-		if(firstNumber > 0 && secondNumber > 0 && secondNumber > INT_MAX - firstNumber)
-		{
-			operationStatus = MathStatus::Overflow;
-			return 0;
-		}
+        int add(int firstNumber, int secondNumber, MathStatus& operationStatus)
+        {
+                if(firstNumber > 0 && secondNumber > 0 && secondNumber > INT_MAX - firstNumber)
+                {
+                        operationStatus = MathStatus::Overflow;
+                        return 0;
+                }
 
                 if(firstNumber < 0 && secondNumber < 0 && firstNumber < INT_MIN - secondNumber)
                 {
                         operationStatus = MathStatus::Overflow;
-			return 0;
+                        return 0;
                 }
 
-		return firstNumber + secondNumber;
-	}
-	
-	int substract(int firstNumber, int secondNumber, MathStatus& operationStatus)
-	{
-		if(firstNumber > 0 && secondNumber < 0 &&  firstNumber > INT_MAX + secondNumber)
+                return firstNumber + secondNumber;
+        }
+
+        int substract(int firstNumber, int secondNumber, MathStatus& operationStatus)
+        {
+                if(firstNumber > 0 && secondNumber < 0 &&  firstNumber > INT_MAX + secondNumber)
                 {
                         operationStatus = MathStatus::Overflow;
                         return 0;
@@ -48,71 +47,79 @@ namespace math
                         return 0;
                 }
 
-		return firstNumber - secondNumber;
-	}
-	
-	int multiply(int firstNumber, int secondNumber, MathStatus& operationStatus)
-	{
-		long long result = static_cast<long long>(firstNumber) * secondNumber;
-		if(result > INT_MAX || result < INT_MIN)
-		{
-		operationStatus = MathStatus::Overflow;
-		return 0;
-		}
-		return firstNumber * secondNumber;
-	}
-
-	int divide(int firstNumber, int secondNumber, MathStatus& operationStatus)
-	{
-		if(secondNumber == 0)
-		{
-		 operationStatus = MathStatus::DivideByZero;
-		 return 0;
-		}
-
-		if(firstNumber == INT_MIN && secondNumber == -1)
-		{
-		        operationStatus = MathStatus::Overflow;
-                        return 0;
-		}
-		
-
-		return firstNumber / secondNumber;
-	}
-
-	int power(int firstNumber, int secondNumber, MathStatus& operationStatus)
-        {
-		long long result = 1;
-                for(int i = 0; i < secondNumber; ++i)
-                {
-			result *= firstNumber;
-                	if(result > INT_MAX || result < INT_MIN)
-                	{
-                		operationStatus = MathStatus::Overflow;
-                		return 0;
-                	}
-                }
-
-		return static_cast<int>(result);
+                return firstNumber - secondNumber;
         }
 
-        int factorial(int firstNumber, MathStatus& operationStatus)
+        int multiply(int firstNumber, int secondNumber, MathStatus& operationStatus)
+        {
+                long long result = static_cast<long long>(firstNumber) * secondNumber;
+                if(result > INT_MAX || result < INT_MIN)
+                {
+                operationStatus = MathStatus::Overflow;
+                return 0;
+                }
+                return firstNumber * secondNumber;
+        }
+
+        int divide(int firstNumber, int secondNumber, MathStatus& operationStatus)
+        {
+                if(secondNumber == 0)
+                {
+                 operationStatus = MathStatus::DivideByZero;
+                 return 0;
+                }
+
+                if(firstNumber == INT_MIN && secondNumber == -1)
+                {
+                        operationStatus = MathStatus::Overflow;
+                        return 0;
+                }
+
+
+                return firstNumber / secondNumber;
+        }
+
+        int power(int firstNumber, int secondNumber, MathStatus& operationStatus)
         {
                 long long result = 1;
-		if (firstNumber < 0)
-		{
-    			operationStatus = MathStatus::FactorialFromNegative;
-    			return 0;
-		}
-
-                for(int i = firstNumber; i > 0; --i)
+                for(int i = 0; i < secondNumber; ++i)
                 {
-			result *= i;
+                        result *= firstNumber;
                         if(result > INT_MAX || result < INT_MIN)
                         {
                                 operationStatus = MathStatus::Overflow;
                                 return 0;
                         }
+                }
+
+                return static_cast<int>(result);
+        }
+
+        int factorial(int firstNumber, MathStatus& operationStatus)
+        {
+                if (firstNumber < 0)
+                {
+                        operationStatus = MathStatus::FactorialFromNegative;
+                        return 0;
+                }
+
+                if (operationStatus != MathStatus::Ok)
+                {
+                        return 0;
+                }
+
+                if(firstNumber == 1 || firstNumber == 0)
+                {
+                        return 1;
+                }
+
+                long long result = static_cast<long long>(firstNumber) * factorial(firstNumber - 1, operationStatus);
+
+
+                if(result > INT_MAX || result < INT_MIN)
+                {
+                        operationStatus = MathStatus::Overflow;
+                        return 0;
                 }
 
                 return static_cast<int>(result);
